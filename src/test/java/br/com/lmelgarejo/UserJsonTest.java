44a0a288e9d2fd599f2dd83a2804dad6531573fe
Leonardo Melgarejo <leonardo_melgarejo@hotmail.com>
2024-08-15 19:40:36 +0100

    Allure report adicionado

--- src/test/java/br/com/lmelgarejo/UserJsonTest.java
+++ src/test/java/br/com/lmelgarejo/UserJsonTest.java
@@ -1,5 +1,7 @@
 package br.com.lmelgarejo;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
 import io.restassured.http.Method;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
@@ -13,22 +15,25 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Feature("Testar Json da API")
 public class UserJsonTest {
 
     @Test
+    @Description("Deve verificar primeiro nivel do json")
     public void deveVerificarPrimeiroNivel(){
         given()
-                .when()
-                .get("https://restapi.wcaquino.me/users/1")
-                .then()
-                .statusCode(200)
-                .body("id", is(1))
-                .body("name", containsString("Silva"))
-                .body("age", greaterThan(18))
+        .when()
+            .get("https://restapi.wcaquino.me/users/1")
+        .then()
+            .statusCode(200)
+            .body("id", is(1))
+            .body("name", containsString("Silva"))
+            .body("age", greaterThan(18))
         ;
     }
 
     @Test
+    @Description("Deve verificar primeiro nivel do json de outras formas")
     public void deveVerificarPrimeiroNivelOutrasFormas(){
         Response response = request(Method.GET, "https://restapi.wcaquino.me/users/1");
 
@@ -43,67 +48,72 @@ public class UserJsonTest {
     }
 
     @Test
+    @Description("Deve verificar segundo nivel do json")
     public void deveVerificarSegundoNivel(){
         given()
-                .when()
-                .get("https://restapi.wcaquino.me/users/2")
-                .then()
-                .statusCode(200)
-                .body("name", containsString("Joaquina"))
-                .body("endereco.rua", is("Rua dos bobos"))
+        .when()
+            .get("https://restapi.wcaquino.me/users/2")
+        .then()
+            .statusCode(200)
+            .body("name", containsString("Joaquina"))
+            .body("endereco.rua", is("Rua dos bobos"))
         ;
     }
 
     @Test
+    @Description("Deve verificar lista no json")
     public void deveVerificarLista(){
         given()
-                .when()
-                .get("https://restapi.wcaquino.me/users/3")
-                .then()
-                .statusCode(200)
-                .body("filhos", hasSize(2))
-                .body("filhos[0].name", is("Zezinho"))
-                .body("filhos[1].name", is("Luizinho"))
-                .body("filhos.name", hasItem("Zezinho"))
-                .body("filhos.name", hasItems("Zezinho","Luizinho"))
+        .when()
+            .get("https://restapi.wcaquino.me/users/3")
+        .then()
+            .statusCode(200)
+            .body("filhos", hasSize(2))
+            .body("filhos[0].name", is("Zezinho"))
+            .body("filhos[1].name", is("Luizinho"))
+            .body("filhos.name", hasItem("Zezinho"))
+            .body("filhos.name", hasItems("Zezinho","Luizinho"))
         ;
     }
 
     @Test
+    @Description("Deve retornar erro de usuário inexistente")
     public void deveRetornarErroUsuarioInexistente(){
         given()
-                .when()
-                .get("https://restapi.wcaquino.me/users/4")
-                .then()
-                .statusCode(404)
-                .body("error", is("Usuário inexistente"))
+        .when()
+            .get("https://restapi.wcaquino.me/users/4")
+        .then()
+            .statusCode(404)
+            .body("error", is("Usuário inexistente"))
         ;
     }
 
     @Test
+    @Description("Deve verificar lista na raiz do json")
     public void deveVerificarListaRaiz(){
         given()
-                .when()
-                .get("https://restapi.wcaquino.me/users")
-                .then()
-                .statusCode(200)
-                .body("$", hasSize(3))
-                .body("name", hasItems("João da Silva","Maria Joaquina","Ana Júlia"))
-                .body("age[1]", is(25))
-                .body("filhos.name", hasItem(Arrays.asList("Zezinho","Luizinho")))
-                .body("salary", contains(1234.5678f, 2500, null))
+        .when()
+            .get("https://restapi.wcaquino.me/users")
+        .then()
+            .statusCode(200)
+            .body("$", hasSize(3))
+            .body("name", hasItems("João da Silva","Maria Joaquina","Ana Júlia"))
+            .body("age[1]", is(25))
+            .body("filhos.name", hasItem(Arrays.asList("Zezinho","Luizinho")))
+            .body("salary", contains(1234.5678f, 2500, null))
         ;
     }
 
     @Test
+    @Description("Deve unir json com Java")
     public void devoUnirJsonPathComJAVA(){
         ArrayList<String> nomes =
                 given()
-                        .when()
-                        .get("https://restapi.wcaquino.me/users")
-                        .then()
-                        .statusCode(200)
-                        .extract().path("name.findAll{it.startsWith('Maria')}")
+                .when()
+                    .get("https://restapi.wcaquino.me/users")
+                .then()
+                    .statusCode(200)
+                    .extract().path("name.findAll{it.startsWith('Maria')}")
                 ;
         assertEquals(1, nomes.size());
         assertTrue(nomes.get(0).equalsIgnoreCase("mAria Joaquina"));
