c8b63737d04e400219200cd4fb1793cedb2a1a38
Leonardo Melgarejo <leonardo_melgarejo@hotmail.com>
2024-08-15 15:59:22 +0100

    Mensagem de erro

--- src/test/java/br/com/lmelgarejo/UserJsonTest.java
+++ src/test/java/br/com/lmelgarejo/UserJsonTest.java
@@ -49,4 +49,30 @@ public class UserJsonTest {
             .body("endereco.rua", is("Rua dos bobos"))
         ;
     }
+
+    @Test
+    public void deveVerificarLista(){
+        given()
+        .when()
+            .get("https://restapi.wcaquino.me/users/3")
+        .then()
+            .statusCode(200)
+            .body("filhos", hasSize(2))
+            .body("filhos[0].name", is("Zezinho"))
+            .body("filhos[1].name", is("Luizinho"))
+            .body("filhos.name", hasItem("Zezinho"))
+            .body("filhos.name", hasItems("Zezinho","Luizinho"))
+        ;
+    }
+
+    @Test
+    public void deveRetornarErroUsuarioInexistente(){
+        given()
+        .when()
+            .get("https://restapi.wcaquino.me/users/4")
+        .then()
+            .statusCode(404)
+            .body("error", is("Usuário inexistente"))
+        ;
+    }
 }
