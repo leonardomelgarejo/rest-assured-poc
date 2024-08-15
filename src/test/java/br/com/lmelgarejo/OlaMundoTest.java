2aa8cd188111cbbf0c530424086d028bbfbd758b
Leonardo Melgarejo <leonardo_melgarejo@hotmail.com>
2024-08-15 14:13:13 +0100

    Modo fluente

--- src/test/java/br/com/lmelgarejo/OlaMundoTest.java
+++ src/test/java/br/com/lmelgarejo/OlaMundoTest.java
@@ -7,15 +7,33 @@ import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+
 public class OlaMundoTest {
 
     @Test
     public void testOlaMundo(){
-        Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+        Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
         Assertions.assertEquals("Ola Mundo!", response.getBody().asString());
         Assertions.assertEquals(200, response.statusCode());
 
         ValidatableResponse validacao = response.then();
         validacao.statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerOutrasFormasRestAssured(){
+        Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+
+       get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+
+       given()
+       .when()
+           .get("http://restapi.wcaquino.me/ola")
+       .then()
+           .assertThat()
+               .statusCode(200);
     }
 }
