b74e9a7e2ca1f1fb98333969e5603e583616ee9f
Leonardo Melgarejo <leonardo_melgarejo@hotmail.com>
2024-08-15 17:14:00 +0100

    Update gha-ci.yml

--- src/test/java/br/com/lmelgarejo/UserJsonTest.java
+++ src/test/java/br/com/lmelgarejo/UserJsonTest.java
@@ -18,13 +18,13 @@ public class UserJsonTest {
     @Test
     public void deveVerificarPrimeiroNivel(){
         given()
-        .when()
-            .get("https://restapi.wcaquino.me/users/1")
-        .then()
-            .statusCode(200)
-            .body("id", is(1))
-            .body("name", containsString("Silva"))
-            .body("age", greaterThan(18))
+                .when()
+                .get("https://restapi.wcaquino.me/users/1")
+                .then()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("name", containsString("Silva"))
+                .body("age", greaterThan(18))
         ;
     }
 
@@ -45,66 +45,66 @@ public class UserJsonTest {
     @Test
     public void deveVerificarSegundoNivel(){
         given()
-        .when()
-            .get("https://restapi.wcaquino.me/users/2")
-        .then()
-            .statusCode(200)
-            .body("name", containsString("Joaquina"))
-            .body("endereco.rua", is("Rua dos bobos"))
+                .when()
+                .get("https://restapi.wcaquino.me/users/2")
+                .then()
+                .statusCode(200)
+                .body("name", containsString("Joaquina"))
+                .body("endereco.rua", is("Rua dos bobos"))
         ;
     }
 
     @Test
     public void deveVerificarLista(){
         given()
-        .when()
-            .get("https://restapi.wcaquino.me/users/3")
-        .then()
-            .statusCode(200)
-            .body("filhos", hasSize(2))
-            .body("filhos[0].name", is("Zezinho"))
-            .body("filhos[1].name", is("Luizinho"))
-            .body("filhos.name", hasItem("Zezinho"))
-            .body("filhos.name", hasItems("Zezinho","Luizinho"))
+                .when()
+                .get("https://restapi.wcaquino.me/users/3")
+                .then()
+                .statusCode(200)
+                .body("filhos", hasSize(2))
+                .body("filhos[0].name", is("Zezinho"))
+                .body("filhos[1].name", is("Luizinho"))
+                .body("filhos.name", hasItem("Zezinho"))
+                .body("filhos.name", hasItems("Zezinho","Luizinho"))
         ;
     }
 
     @Test
     public void deveRetornarErroUsuarioInexistente(){
         given()
-        .when()
-            .get("https://restapi.wcaquino.me/users/4")
-        .then()
-            .statusCode(404)
-            .body("error", is("Usuário inexistente"))
+                .when()
+                .get("https://restapi.wcaquino.me/users/4")
+                .then()
+                .statusCode(404)
+                .body("error", is("Usuário inexistente"))
         ;
     }
 
     @Test
     public void deveVerificarListaRaiz(){
         given()
-        .when()
-            .get("https://restapi.wcaquino.me/users")
-        .then()
-            .statusCode(200)
-            .body("$", hasSize(3))
-            .body("name", hasItems("João da Silva","Maria Joaquina","Ana Júlia"))
-            .body("age[1]", is(25))
-            .body("filhos.name", hasItem(Arrays.asList("Zezinho","Luizinho")))
-            .body("salary", contains(1234.5678f, 2500, null))
+                .when()
+                .get("https://restapi.wcaquino.me/users")
+                .then()
+                .statusCode(200)
+                .body("$", hasSize(3))
+                .body("name", hasItems("João da Silva","Maria Joaquina","Ana Júlia"))
+                .body("age[1]", is(25))
+                .body("filhos.name", hasItem(Arrays.asList("Zezinho","Luizinho")))
+                .body("salary", contains(1234.5678f, 2500, null))
         ;
     }
 
     @Test
     public void devoUnirJsonPathComJAVA(){
         ArrayList<String> nomes =
-            given()
-            .when()
-                .get("https://restapi.wcaquino.me/users")
-            .then()
-                .statusCode(200)
-                .extract().path("name.findAll{it.startsWith('Maria')}")
-            ;
+                given()
+                        .when()
+                        .get("https://restapi.wcaquino.me/users")
+                        .then()
+                        .statusCode(200)
+                        .extract().path("name.findAll{it.startsWith('Maria')}")
+                ;
         assertEquals(1, nomes.size());
         assertTrue(nomes.get(0).equalsIgnoreCase("mAria Joaquina"));
         assertEquals(nomes.get(0).toUpperCase(), "maria joaquina".toUpperCase());
